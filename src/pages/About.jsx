3cfc012b40217d49a1640b8bbbb7336e2ddbd57b
jtopyuebi <classicmcnet@163.com>
2024-08-27 23:30:51 +0800

    添加了关于界面 (排版一堆问题

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './/..//css/About.css'; 
 
 const About = () => {
+  const [iframeVisible, setIframeVisible] = useState(false);
+
+  const openWebsite = (url) => {
+    window.open(url, '_blank');
+  };
+
+  const openIframe = () => {
+    setIframeVisible(true);
+  };
+
+  const closeIframe = () => {
+    setIframeVisible(false);
+  };
+
   return (
-  <div style={{ width: '920px', height: '700px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      关于内容
+    <div className="about-container">
+      <div className="image-container">
+        <img src="./../../img/bg.jpg" alt="My Image" />
+      </div>
+      <h1>JTOP游戏启动器</h1>
+      <p>版本：v1.0.1.3a</p>
+      <div className="button-container">
+        <input type="image" src="./../../img/官方网站.png" alt="官方网站" onClick={() => openWebsite('https://jtopgame.top')} />
+        <input type="image" src="./../../img/意见反馈.png" alt="意见反馈" onClick={() => openWebsite('https://jtopgame.top/')} />
+        <input type="image" src="./../../img/开源信息.png" alt="开源信息" id="openIframeButton" onClick={openIframe} />
+      </div>
+
+      {iframeVisible && (
+        <div id="mask" onClick={closeIframe}>
+          <div id="iframeWindow">
+            <div className="closeButton" onClick={closeIframe}>&times;</div>
+            <iframe id="iframeDisplay" src="third-party.html" title="Third Party Content"></iframe>
+          </div>
+        </div>
+      )}
     </div>
-);
-  
+  );
 };
 
 export default About;
