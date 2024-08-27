6b3b701e49ef7124367548d1ecf5c2a6e4eae32c
jtopyuebi <classicmcnet@163.com>
2024-08-27 22:10:54 +0800

    修复所有页面的排版问题

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const About = () => {
-  return <div>关于内容</div>;
+  return (
+  <div style={{ width: '920px', height: '700px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      关于内容
+    </div>
+);
+  
 };
 
 export default About;
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const News = () => {
-  return <div>新闻内容</div>;
+  return (
+    <div style={{ width: '920px', height: '700px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        新闻内容
+      </div>
+  );
 };
 
 export default News;
