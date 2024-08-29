917312ce7c5992ce5f754e5019d3e40d644914b0
hmrBH <1218271192@qq.com>
2024-08-29 16:42:48 +0800

    添加更多日志记录

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{Manager, SystemTray, SystemTrayEvent};
-use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
+use tauri::{Manager, SystemTray, SystemTrayEvent, CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem};
 use std::sync::{Arc, Mutex};
 use simplelog::*;
 use std::fs::File;
@@ -12,7 +11,6 @@ extern crate log;
 
 mod utils;
 
-
 fn main() {
 
     CombinedLogger::init(vec![
@@ -20,7 +18,7 @@ fn main() {
         WriteLogger::new(LevelFilter::Info, Config::default(), File::create("log.txt").unwrap()),
     ]).unwrap();
 
-    log::debug!("创建系统托盘");
+    log::info!("创建系统托盘");
 
     let context = tauri::generate_context!();
     
@@ -35,7 +33,8 @@ fn main() {
     //将托盘菜单添加到 SystemTray 实例
     let tray = SystemTray::new().with_menu(tray_menu);
     // 记录当前窗口是否显示
-    let is_show_window = Arc::new(Mutex::new(false)); 
+    // 窗口默认是显示状态，所以初始值设置为 true
+    let is_show_window = Arc::new(Mutex::new(true)); 
 
     tauri::Builder::default()
         // 注册指令 open_url_in_browser
@@ -45,6 +44,7 @@ fn main() {
             SystemTrayEvent::MenuItemClick { id, .. } => {
               match id.as_str() {
                 "quit" => {
+                  log::info!("由托盘菜单退出程序");
                   std::process::exit(0);
                 }
                 "hide" => {
@@ -57,6 +57,7 @@ fn main() {
                             let item_handle = app.tray_handle().get_item("hide");
                             let _ = item_handle.set_title("显示窗口");
                             *is_show_window = false;
+                            log::debug!("隐藏窗口")
                         }
                         else {
                             if let Err(e) = window.show() {
@@ -65,26 +66,30 @@ fn main() {
                             let item_handle = app.tray_handle().get_item("hide");
                             let _ = item_handle.set_title("隐藏窗口");
                             *is_show_window = true;
+                            log::debug!("显示窗口");
                         }
                     }
                 }
                 _ => {}
               }
             }
-            SystemTrayEvent::LeftClick { .. } => {
+            SystemTrayEvent::DoubleClick { .. } => {
               if let Some(window) = app.get_window("main") {
-                  if let Err(e) = window.show() {
-                      log::error!("无法显示窗口: {}", e)
-                  }
+                log::info!("托盘双击，显示窗口在最上层");
+                // 显示窗口在最上层
+                if let Err(e) = window.set_focus() {
+                    log::error!("无法设置窗口焦点: {}", e);
+                }
               }
-          }
+            }
             _ => {}
           })
         .run(context)
-        .expect("error while running tauri application");
+        .expect("运行 Tauri 应用失败");
 }
 
 #[tauri::command]
 fn open_url_in_browser(url: &str) {
   utils::open_url_in_default_browser(url);
+  log::info!("打开网页：{}", url)
 }
