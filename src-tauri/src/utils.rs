84326150afbffb3293567d89af0a960c49a125b7
hmrBH <1218271192@qq.com>
2024-08-30 13:48:24 +0800

    准备使用python作为后端

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,8 +1,8 @@
+extern crate webbrowser;
+
 pub fn open_url_in_default_browser(url: &str) {
     match webbrowser::open(url) {
         Ok(_) => println!("浏览器已打开"),
         Err(e) => eprintln!("无法打开浏览器: {}", e),
     }
 }
-
-extern crate webbrowser;
